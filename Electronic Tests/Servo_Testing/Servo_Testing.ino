1d3bf34f9d84d969647ab7290ecbb51afa68d2a0
Jensen Tong <jensen.tong@gmail.com>
2021-11-10 10:43:40 +0000

    sharpIR and servo updates

--- Electronic Tests/Servo_Testing/Servo_Testing.ino
+++ Electronic Tests/Servo_Testing/Servo_Testing.ino
@@ -18,20 +18,26 @@
 Servo myservo;  // create servo object to control a servo
 // twelve servo objects can be created on most boards
 
-int pos = 0;    // variable to store the servo position
+int pos = 40;    // variable to store the servo position
 
 void setup() {
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  Serial.begin(9600);
+  
 }
 
 void loop() {
-  for (pos = 0; pos <= 180; pos += 5) { // goes from 0 degrees to 180 degrees
+
+  for (pos = 40; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
   }
-  for (pos = 180; pos >= 0; pos -= 5) { // goes from 180 degrees to 0 degrees
+  for (pos = 180; pos >= 40; pos -= 1) { // goes from 180 degrees to 0 degrees
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
   }
+
+  delay(1000);
+
 }
--- Electronic Tests/SharpIR_Dist_Sensor_Test/SharpIR_Dist_Sensor_Test.ino
+++ Electronic Tests/SharpIR_Dist_Sensor_Test/SharpIR_Dist_Sensor_Test.ino
@@ -11,7 +11,7 @@ Test in progress.
 #include <SharpIR.h>
 
 #define IR A1 // define signal pin
-#define model 20150 // used 1080 because model GP2Y0A21YK0F is used
+#define model 20150 // used 20150 because model GP2Y0A02Y is used
 // Sharp IR code for Robojax.com
 // ir: the pin where your sensor is attached
 // model: an int that determines your sensor:  1080 for GP2Y0A21Y
@@ -19,6 +19,8 @@ Test in progress.
 //                                            430 for GP2Y0A41SK  
 
 SharpIR IRsensor(IR, model);
+
+
 void setup() {
   // Sharp IR code for Robojax.com
   Serial.begin(9600);
@@ -29,15 +31,27 @@ void loop() {
   // Sharp IR code for Robojax.com
   delay(500);   
 
+  float volts = analogRead(IR)*0.0048828125;  // value from sensor * (5/1024)
+  int distance = 13*pow(volts, -1); // worked out from datasheet graph
+  delay(1000); // slow down serial port 
+  
+  if (distance <= 30){
+    Serial.println(distance);   // print the distance
+  }
 
+
+
+
+
+  /* Sharp IR code for Robojax.com
   
-  
-  unsigned long startTime=millis();  // takes the time before the loop on the library begins
+    unsigned long startTime=millis();  // takes the time before the loop on the library begins
 
   int dis = IRsensor.getDistance();  // this returns the distance to the object you're measuring
-  Serial.println(dis);
+  //Serial.println(dis);
 
   long a1 = analogRead(A1);
+  Serial.println(a1);
   int distance = 9462/(analogRead(A1)-16.92);
   Serial.println(dis);
 
