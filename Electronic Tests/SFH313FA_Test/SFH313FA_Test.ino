e0c43f112999a9895d015f4ffd496ff2be1eec2b
Jensen Tong <jensen.tong@gmail.com>
2021-11-21 16:01:23 +0000

    SFH313FA Photo Transistor Working!!!
    only use digitalCode(), as it can count up to 60kHz, where analogCode() can do only up to 2kHz.

--- Electronic Tests/SFH313FA_Test/SFH313FA_Test.ino
+++ Electronic Tests/SFH313FA_Test/SFH313FA_Test.ino
@@ -1,11 +1,11 @@
-bool state = false;
-int prev_reading;
+#include "storage.h"
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   Serial.println("\t");
-  pinMode(A0, INPUT);
+  pinMode(A0, INPUT); // analog pin
+  pinMode(2, INPUT); // digital pin
   prev_reading = analogRead(A0);
 }
 
@@ -14,30 +14,6 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-  //Serial.println(analogRead(A0));
-
-  
-  int count = 0;
-  int smallcount = 0;
-  int prevtime = millis();
-  while (millis() - prevtime < 1000){
-    while (analogRead(A0)> 200){
-        int prev = analogRead(A0);
-        if (analogRead(A0) > prev){
-          state = true;
-        }
-        if (analogRead(A0) < prev){
-          state = false;
-          smallcount += 1;
-        }
-      }
-    while (analogRead(A0)< 200){}
-    count += 1;
-  }
-  Serial.print(count);
-  Serial.print('\t');
-  Serial.println(smallcount);
-
+  digitalCode();
 
 }
--- Electronic Tests/SFH313FA_Test/storage.h
+++ Electronic Tests/SFH313FA_Test/storage.h
@@ -1,3 +1,7 @@
+int d;
+int prev_reading;
+bool state = false;
+
 int analogReadDerivative() {
   int _read = analogRead(A0);
   d = _read - prev_reading;
@@ -5,6 +9,66 @@ int analogReadDerivative() {
   return d;
 }
 
-int countSmall() {
-  
+void analogCodeBigSmall() {
+
+  int count = 0;
+  int smallcount = 0;
+  int prevtime = millis();
+  while (millis() - prevtime < 1000){
+    while (analogRead(A0)> 200){
+        int prev = analogRead(A0);
+        if (analogRead(A0) > prev){
+          state = true;
+        }
+        if (analogRead(A0) < prev){ 
+          state = false;
+          smallcount += 1;
+        }
+      }
+    while (analogRead(A0)< 200){}
+    count += 1;
+  }
+  Serial.print(count);
+  Serial.print('\t');
+  Serial.println(smallcount);
+}
+
+void digitalCode(){
+    //Serial.println(digitalRead(2));
+
+  int count = 0;
+  int prevtime = millis();
+  bool state = false;
+  while (millis() - prevtime <= 100){ // rollover issue to solve
+    if (digitalRead(2)== HIGH && state == false){
+      state = true;
+    }
+    if (digitalRead(2)== LOW && state == true){
+      state = false;
+      count += 1;  
+    }
+  }
+  Serial.println(count);
+  //delay(500);
+
+}
+
+
+void analogCode(){
+
+  int count = 0;
+  int prevtime = millis();
+  bool state = false;
+  while (millis() - prevtime <= 1000){ // rollover issue to solve
+    if (analogRead(A0) > 500 && state == false){
+      state = true;
+    }
+    if (analogRead(A0) < 500 && state == true){
+      state = false;
+      count += 1;  
+    }
+  }
+  Serial.println(count);
+  delay(500);
+
 }
